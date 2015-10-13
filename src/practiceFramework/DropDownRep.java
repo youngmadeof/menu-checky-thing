a145e1aa3d1269e4fa445ad7b6da853f59426763
Mark Raper <mrkraper@gmail.com>
2015-10-13 21:14:21 +0100

    gui messing

--- src/practiceFramework/DropDownRep.java
+++ src/practiceFramework/DropDownRep.java
@@ -3,9 +3,6 @@ package practiceFramework;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-//import java.awt.FlowLayout;
-
-
 import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
@@ -14,79 +11,89 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 
 public class DropDownRep extends JFrame{
-	
+
 	public DropDownRep(){
-		
+
 		prepareGUI();
 	}
-	
+
 
 	public final void prepareGUI(){
-		
+
 		JPanel main = new JPanel();
 		main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
 		add(main);
-		
+
 		JPanel fieldPanel = new JPanel(new BorderLayout());
-		fieldPanel.setMaximumSize(new Dimension (400,100));
-		JLabel tempfields = new JLabel("This is where the fields will go");
-		tempfields.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-		fieldPanel.add(tempfields);
-		
-		JSeparator separator = new JSeparator();
-		separator.setForeground(Color.BLUE);
-		
-		fieldPanel.add(separator, BorderLayout.SOUTH);
-		
+		fieldPanel.setMaximumSize(new Dimension (400, 100));
+		JTextField urlText = new JTextField();
+		JTextField idText = new JTextField();
+
+		fieldPanel.setBorder(BorderFactory.createEmptyBorder(15, 35, 15, 35));
+		fieldPanel.add(urlText, BorderLayout.NORTH);
+		fieldPanel.add(idText, BorderLayout.SOUTH);		
+
 		main.add(fieldPanel);
-		
-		JPanel listPanel = new JPanel(new BorderLayout());
-		listPanel.setMaximumSize(new Dimension (400,300));
-		JLabel tempList = new JLabel("Stick output list here!");
-		tempList.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-		listPanel.add(tempList);
-		
-		main.add(listPanel);
-		
-		JPanel buttPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));		
+
+		JScrollPane pane = new JScrollPane();
+		JTextArea outArea = new JTextArea();
+
+		JPanel textPanel = new JPanel(new BorderLayout());
+		textPanel.setMaximumSize(new Dimension (400,300));
+		textPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+		//set word wrap
+		outArea.setLineWrap(true);
+		//wrap at word boundaries - white spaces
+		outArea.setWrapStyleWord(true);
+		outArea.setEditable(false);
+
+		//make scrollable
+		pane.getViewport().add(outArea);
+		textPanel.add(outArea);
+
+		main.add(textPanel);
+
+		JPanel buttPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		JButton submit = new JButton("Submit");
 		JButton clear = new JButton ("Clear");
 		JButton close = new JButton ("Close");
-		
+
 		buttPanel.add(submit);
 		buttPanel.add(clear);
 		buttPanel.add(close);
 		main.add(buttPanel);
-		
-		buttPanel.setMaximumSize(new Dimension (400, 50));
-		
-		
+
+		buttPanel.setMaximumSize(new Dimension (385, 40));
+
+
 		setTitle("menu checky thing");
 		setSize(400, 400);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
+
+
 	}
 	public static void main(String[] args) {
-		
+
 		SwingUtilities.invokeLater(new Runnable(){
-			
+
 		public void run(){
 		DropDownRep drop = new DropDownRep();
 		drop.setVisible(true);
 		}
 		});
-		
+
 
 	}
-	
-	
+
+
 
 }
